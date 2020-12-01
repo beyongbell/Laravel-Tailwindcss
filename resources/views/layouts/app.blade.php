4271bd4d2e52b7d7c6544e7b2739f751558e66b6
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-12-01 17:36:40 +0700

    Update Post Index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,35 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Posty</title>
 </head>
-<body>
+<body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3"> Home </a>
+            </li>
+            <li>
+                <a href="" class="p-3"> Dashboard </a>
+            </li>
+            <li>
+                <a href="" class="p-3"> Post </a>
+            </li>
+        </ul>
+
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3"> Tinnakorn Choompee </a>
+            </li>
+            <li>
+                <a href="" class="p-3"> Register </a>
+            </li>
+            <li>
+                <a href="" class="p-3"> Login </a>
+            </li>
+            <li>
+                <a href="" class="p-3"> Logout </a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
