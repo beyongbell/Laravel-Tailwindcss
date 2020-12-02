c81602ddbbb8a43e0396f3c85d7f6748ffa8718d
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-12-02 10:51:01 +0700

    Create Register Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/register', [RegisterController::class, 'index']);
 
 Route::get('/posts', function () {
     return view('posts.index');
